15f40dfcf90e9fae8ca9d1f3f3d94d8d4d06da4e
jcoulaud <j.coulaud@gmail.com>
2018-10-28 17:32:17 +0100

    add script oribi.io for analytics

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
-import Paper from 'material-ui/Paper';
-import Layout from 'material-ui/Layout';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import Paper from "material-ui/Paper";
+import Layout from "material-ui/Layout";
+import { Link } from "react-router-dom";
 
-import Languages from './../languages/languages';
+import Languages from "./../languages/languages";
 
 class ListCategories extends Component {
+	componentDidMount() {
+		const s = document.createElement("script");
+		s.type = "text/javascript";
+		s.async = true;
+		s.innerHTML =
+			'(function(b, o, n, g, s, r, c) { if (b[s]) return; b[s] = {}; b[s].scriptToken = "XzYxNjA3NzI2MA"; r = o.createElement(n); c = o.getElementsByTagName(n)[0]; r.async = 1; r.src = g; r.id = s + n; c.parentNode.insertBefore(r, c); })(window, document, "script", "//cdn.oribi.io/XzYxNjA3NzI2MA/oribi.js", "ORIBI");';
+		this.instance.appendChild(s);
+	}
+
 	render() {
-	  const languagesCategories = Languages.map((language) => {
-	    return (
-	    	<Layout key={language.lang.toLowerCase()} className="ListLanguages-item" item xs={12} sm={4} md={3}>
-	    		<Link to={"/language/" + language.lang.toLowerCase()}>
+		const languagesCategories = Languages.map(language => {
+			return (
+				<Layout key={language.lang.toLowerCase()} className="ListLanguages-item" item xs={12} sm={4} md={3}>
+					<Link to={"/language/" + language.lang.toLowerCase()}>
 						<Paper className="ListLanguages-paper">
-							<i className={language.icon}></i>
+							<i className={language.icon} />
 							<div>
 								<span>{language.lang}</span>
 							</div>
 						</Paper>
 					</Link>
 				</Layout>
-	    );
-	  });
+			);
+		});
 
-	  return (
-	  	<Layout container className="ListLanguages-root">
-				{languagesCategories}
-		  </Layout>
-	  );
+		return (
+			<div ref={el => (this.instance = el)}>
+				<Layout container className="ListLanguages-root">
+					{languagesCategories}
+				</Layout>
+			</div>
+		);
 	}
 }
 
