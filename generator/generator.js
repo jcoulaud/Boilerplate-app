1654ad360dea778013aae56714f66289b8f606cd
Julien Coulaud <j.coulaud@gmail.com>
2020-03-24 11:35:01 +0100

    update modules + update languages

--- generator/generator.js
+++ generator/generator.js
@@ -1,22 +1,22 @@
 /**
  * Run to create 'src/languages/name_of_language.json' output files.
  */
-require('dotenv').config();
-const jsonfile = require('jsonfile');
-const async = require('async');
-const path = require('path');
-const Octokit = require('@octokit/rest');
-const fs = require('fs');
+require("dotenv").config();
+const jsonfile = require("jsonfile");
+const async = require("async");
+const path = require("path");
+const { Octokit } = require("@octokit/rest");
+const fs = require("fs");
 
-const languagesDirectory = path.join(__dirname, '..', 'src', 'languages');
-const Languages = require('./../src/languages/languages');
+const languagesDirectory = path.join(__dirname, "..", "src", "languages");
+const Languages = require("./../src/languages/languages");
 
 // Remove old languages files
 fs.readdir(languagesDirectory, (err, files) => {
   if (err) throw err;
 
   for (const file of files) {
-    if (file !== 'languages.js') {
+    if (file !== "languages.js") {
       fs.unlinkSync(path.join(languagesDirectory, file), err => {
         if (err) throw err;
       });
@@ -27,32 +27,32 @@ fs.readdir(languagesDirectory, (err, files) => {
 // Init Github API
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
-  userAgent: process.env.GITHUB_USER_AGENT,
+  userAgent: process.env.GITHUB_USER_AGENT
 });
 
 // Get infos about Github URL
 function getGithubPath(url) {
-  return url.replace('https://github.com/', '');
+  return url.replace("https://github.com/", "");
 }
 function getGithubOwner(url) {
-  return getGithubPath(url).split('/')[0];
+  return getGithubPath(url).split("/")[0];
 }
 function getGithubName(url) {
-  return getGithubPath(url).split('/')[1];
+  return getGithubPath(url).split("/")[1];
 }
 
 function growRepo(repo, outerCallback) {
   const repoInfo = {
     githubUrl: repo,
     githubOwner: getGithubOwner(repo),
-    githubName: getGithubName(repo),
+    githubName: getGithubName(repo)
   };
 
   function getRepo(callback) {
     octokit.repos
       .get({
         owner: repoInfo.githubOwner,
-        repo: repoInfo.githubName,
+        repo: repoInfo.githubName
       })
       .then(results => callback(results));
   }
@@ -70,7 +70,7 @@ function growRepo(repo, outerCallback) {
       githubStars: data.stargazers_count,
       githubWatchers: data.subscribers_count,
       githubCreated_at: data.created_at,
-      githubUpdated_at: data.updated_at,
+      githubUpdated_at: data.updated_at
     };
     outerCallback(null, tree);
   });
@@ -82,7 +82,7 @@ function growRepos(repoList, callback) {
     (repo, cb) => {
       growRepo(repo, cb);
     },
-    callback,
+    callback
   );
 }
 
@@ -91,10 +91,10 @@ Languages.map(language => {
     // Append response to file
     const jsonOutputPath = path.join(
       __dirname,
-      '..',
-      'src',
-      'languages',
-      `${language.lang.toLowerCase()}.json`,
+      "..",
+      "src",
+      "languages",
+      `${language.lang.toLowerCase()}.json`
     );
     jsonfile.writeFileSync(jsonOutputPath, trees, { spaces: 2 });
   });
